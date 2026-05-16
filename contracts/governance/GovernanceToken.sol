0ab40313e43f3e74abaae969e41fd80149c0d04d
MuzzOne333 <241318@astanait.edu.kz>
2026-05-16 14:11:06 +0500

    Fixed some errors

--- contracts/governance/GovernanceToken.sol
+++ contracts/governance/GovernanceToken.sol
@@ -4,9 +4,16 @@ pragma solidity ^0.8.0;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-//Muzaffar
+// Muzaffar
 contract GovernanceToken is ERC20, Ownable {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+
+    constructor(
+        string memory name,
+        string memory symbol
+    )
+        ERC20(name, symbol)
+        Ownable(msg.sender)
+    {}
 
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
@@ -16,4 +23,3 @@ contract GovernanceToken is ERC20, Ownable {
         _burn(from, amount);
     }
 }
-//.
